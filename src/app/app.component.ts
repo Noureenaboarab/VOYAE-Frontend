b0f38f6b9e92cd6c4ffc7921d828af060cd40244
Noureenaboarab <noureenaboarab2003@gmail.com>
2026-06-24 21:30:47 +0300

    added search modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,24 @@ import { RouterOutlet } from '@angular/router';
 import { AnnouncementBarComponent } from './core/components/announcement-bar/announcement-bar.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { FooterComponent } from './core/components/footer/footer.component';
+import { SearchModalComponent } from './shared/components/ui/search-modal/search-modal.component';
 
 @Component({
   selector: 'voy-root',
   standalone: true,
-  imports: [RouterOutlet, AnnouncementBarComponent, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    AnnouncementBarComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchModalComponent,
+  ],
   template: `
     <voy-announcement-bar />
     <voy-header />
     <router-outlet />
     <voy-footer />
+    <voy-search-modal />
   `,
 })
 export class AppComponent {}
